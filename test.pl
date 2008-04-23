6539643df57d2a5d4223d95e948e4315c081da8c
cal <cal@ac7c8259-0de0-4f3c-bb6a-12394caf8efd>
2008-04-23 05:50:30 +0000

    tagged 0.06

--- test.pl
+++ test.pl
@@ -1,5 +1,5 @@
 use Test;
-BEGIN { plan tests => 3 };
+BEGIN { plan tests => 8 };
 
 use Flickr::API;
 ok(1); #
@@ -31,6 +31,30 @@ ok($rsp->{error_code} == 0); # this error code will change in future!
 # check the signing works properly
 #
 
-my $sig = $api->sign_args({'foo' => 'bar'});
+ok('466cd24ced0b23df66809a4d2dad75f8' eq $api->sign_args({'foo' => 'bar'}));
+ok('f320caea573c1b74897a289f6919628c' eq $api->sign_args({'foo' => undef}));
 
-ok($sig eq '466cd24ced0b23df66809a4d2dad75f8');
+
+##################################################
+#
+# check the auth url generator is working
+#
+
+my $uri = $api->request_auth_url('r', 'my_frob');
+
+ok($uri->query eq 'api_sig=d749e3a7bd27da9c8af62a15f4c7b48f&perms=r&frob=my_frob&api_key=made_up_key');
+ok($uri->path eq '/services/auth');
+ok($uri->host eq 'flickr.com');
+ok($uri->scheme eq 'http');
+
+
+##################################################
+#
+# check we can't generate a url without a secret
+#
+
+$api = new Flickr::API({'key' => 'key'});
+$uri = $api->request_auth_url('r', 'frob');
+
+ok(!defined $uri);
+
