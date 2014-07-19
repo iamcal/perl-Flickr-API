d58244631c3d1be272840f60cc851333127ca42a
Steven N. Severinghaus <sns@severinghaus.org>
2014-07-19 13:33:09 -0500

    Warn and return if perms is not passed to request_auth_url

--- lib/Flickr/API.pm
+++ lib/Flickr/API.pm
@@ -71,6 +71,8 @@ sub request_auth_url {
 	my $frob  = shift;
 
 	return undef unless defined $self->{api_secret} && length $self->{api_secret};
+    warn "The 'perms' parameter must be one of: read, write, delete"
+        and return unless $perms && $perms =~ /^(read|write|delete)$/;
 
 	my %args = (
 		'api_key' => $self->{api_key},
