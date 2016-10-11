4eb66e497d95ac5d84f86f80c41d81364c457181
Louis B. Moore <lbmoore@hethcote.com>
2016-10-10 19:02:36 -0600

    Clean up the documentation

--- lib/Flickr/API/Upload.pm
+++ lib/Flickr/API/Upload.pm
@@ -163,47 +163,29 @@ __END__
 
 =head1 NAME
 
-Flickr::API::Request - A request to the Flickr API
+Flickr::API::Upload - An upload via the Flickr API
 
 =head1 SYNOPSIS
 
 =head2 Using the OAuth form:
 
   use Flickr::API;
-  use Flickr::API::Request;
+  use Flickr::API::Upload;
 
-  my $api = Flickr::API->new({'consumer_key' => 'your_api_key'});
+  my $api = Flickr::API->import_storable_config($config_file);
 
-  my $request = Flickr::API::Request->new({
-      'method' => $method,
-      'args' => {},
+  my $response = Flickr::API->upload({
+               'photo'       => 'path/to/photo,
+                'async'       => 0,
+                'title'       => 'My vacation picture',
+                'description' => 'Here I am, riding a horse',
   });
 
-  my $response = $api->execute_request($request);
-
-=head2 Using the original Flickr form:
-
-  use Flickr::API;
-  use Flickr::API::Request;
-
-  my $api = Flickr::API->new({'key' => 'your_api_key'});
-
-  my $request = Flickr::API::Request->new({
-      'method' => $method,
-      'args' => {},
-  });
-
-  my $response = $api->execute_request($request);
 
 
 =head1 DESCRIPTION
 
-This object encapsulates a request to the Flickr API.
-
-C<Flickr::API::Request> is a subclass of L<HTTP::Request>, so you can access
-any of the request parameters and tweak them yourself. The content, content-type
-header and content-length header are all built from the 'args' list by the
-C<Flickr::API::execute_request()> method.
+This modules does most of the work for the upload method in Flickr::API.
 
 
 =head1 AUTHOR
